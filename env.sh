400b521cb09eb498174117cd5489a9c749dbffc3
Cheng TAN <naizhengtan@gmail.com>
2020-09-03 11:56:58 -0400

    env scripts

---
+++ env.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+
+export COBRA_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
+
+function fail {
+  echo "[FAIL] error: $1"
+  exit 1
+}
+
+#=======log files==========
+COBRA_LOG_DIR="/tmp/cobra/log/"
+COBRA_ERR_LOG="/tmp/cobra/error.log"
+COBRA_BENCH_LOG="/tmp/cobra/benchmark.log"
